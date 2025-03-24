d8b98ec567445cafd65f2089bb4ef8048490e4ec
HHKProgrammer <helin@DESKTOP-AE9GKSP>
2025-03-24 22:00:16 +0200

    trying level 7

--- exercise2/tdd-mooc-tetris/src/Tetromino.mjs
+++ exercise2/tdd-mooc-tetris/src/Tetromino.mjs
@@ -1,16 +1,28 @@
 import { RotatingShape } from "./RotatingShape.mjs";
 
 export class Tetromino {
-    constructor(shape) {
+    /*constructor(shape) {
         this.shape = shape;
+    }*/
+    constructor(rotations) {
+        this.rotations = rotations;
+        this.rotationIndex = 0;
+    }
+    get matrix() {
+        return this.rotations[this.rotationIndex];
     }
 
+    rotateRight() {
+        const newTetro = new Tetromino(this.rotations);
+        newTetro.rotationIndex = (this.rotationIndex + 1) % this.rotations.length;
+        return newTetro;
+    }
     rotateClockwise() {
         return new Tetromino(this.shape.rotateClockwise());
     }
 
     toString() {
-        return this.shape.toString();
+        return this.matrix.map(row => row.join("")).join("\n") + "\n";
     }
 
     static T() {
@@ -37,5 +49,28 @@ export class Tetromino {
      .OO
      ...`
     );
+
+    export const T_SHAPE = [
+        [
+            [".", "T", "."],
+            ["T", "T", "T"],
+            [".", ".", "."]
+        ],
+        [
+            [".", "T", "."],
+            [".", "T", "T"],
+            [".", "T", "."]
+        ],
+        [
+            [".", ".", "."],
+            ["T", "T", "T"],
+            [".", "T", "."]
+        ],
+        [
+            [".", "T", "."],
+            ["T", "T", "."],
+            [".", "T", "."]
+        ],
+    ];
 }
 
